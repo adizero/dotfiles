82b63772797b3d80edb8aba90342e51a15001511
Adrian Kocis <adrian.kocis@nokia.com>
2025-07-02 16:03:37 -0700

    Tweak polybar height on different machines

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,6 +9,11 @@ killall -9 -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# DPI detection relies on Xft.dpi setting in .Xresources
+DPI=$(xrdb -query | grep Xft.dpi | awk '{print $2}')
+DPI=${DPI:-96}
+export DPI
+
 short_hostname="${HOSTNAME%%.*}"
 if [ "${short_hostname}" == "hp6550b" ]; then
     # ARCH HP6550B NOTEBOOK
@@ -17,12 +22,14 @@ if [ "${short_hostname}" == "hp6550b" ]; then
     export MODLIST="system-usb-udev filesystem-root memory xbacklight pipewire-rofi-output
     pipewire-rofi-input wifi dropbox eth load-average cpu-cores custom-battery temperature fans date custom-redshift
     openweathermap-fullfeatured powermenu"
+    export POLYBAR_HEIGHT=$(((18 - 0) * DPI / 96))
 elif [ "${short_hostname}" == "probook" ]; then
     # GENTOO HP6550B NOTEBOOK
     export MONITOR=LVDS-1
     # export ETH=enp0s25
     export MODLIST="system-usb-udev filesystem-root memory backlight wifi dropbox eth load-average cpu-cores custom-battery temperature fans date custom-redshift
     openweathermap-fullfeatured powermenu"
+    export POLYBAR_HEIGHT=$(((18 - 0) * DPI / 96))
 elif [[ "${short_hostname}" == "mvakocis"* ]]; then
     # CENTOS 7 DEVPC DESKTOP
     export MONITOR=DP-1
@@ -31,13 +38,8 @@ elif [[ "${short_hostname}" == "mvakocis"* ]]; then
     export MODLIST="removable_disks system-usb-udev filesystem-root-and-home memory xbacklight
     pulseaudio-rofi-output pulseaudio-rofi-input wifi eth load-average cpu-cores battery temperature date
     custom-redshift openweathermap-fullfeatured powermenu"
+    export POLYBAR_HEIGHT=$(((18 - 1) * DPI / 96))
 fi
-
-# DPI detection relies on Xft.dpi setting in .Xresources
-DPI=$(xrdb -query | grep Xft.dpi | awk '{print $2}')
-DPI=${DPI:-96}
-export DPI
-export POLYBAR_HEIGHT=$((18 * DPI / 96))
 
 # Replace newlines with spaces
 MODLIST="${MODLIST//$'\n'/ }"
