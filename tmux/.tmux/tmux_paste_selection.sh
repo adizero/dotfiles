4bbb5114b189645470ac8ba0f6e92b149c570251
Adrian Kocis <adrian.kocis@gmail.com>
2025-07-18 22:24:13 -0700

    Fix tmux paste selection fallback when no DISPLAY or no xsel

--- tmux/.tmux/tmux_paste_selection.sh
+++ tmux/.tmux/tmux_paste_selection.sh
@@ -3,5 +3,5 @@
 if [ -n "${DISPLAY}" ] && command -v xsel &>/dev/null; then
     tmux set-buffer -b primary_selection "$(xsel -o)"; tmux paste-buffer -b primary_selection; tmux delete-buffer -b primary_selection
 else
-    tmux paste-buffer -b primary_selection
+    tmux paste-buffer -p
 fi
