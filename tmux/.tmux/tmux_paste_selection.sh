8d8efa4a0fc5029d8bd0216399d15c53aad9c87c
Adrian Kocis <adrian.kocis@gmail.com>
2025-07-18 21:50:25 -0700

    Consistent paste selection and paste clipboard in alacritty/tmux/chrome

---
+++ tmux/.tmux/tmux_paste_selection.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ -n "${DISPLAY}" ] && command -v xsel &>/dev/null; then
+    tmux set-buffer -b primary_selection "$(xsel -o)"; tmux paste-buffer -b primary_selection; tmux delete-buffer -b primary_selection
+else
+    tmux paste-buffer -b primary_selection
+fi
