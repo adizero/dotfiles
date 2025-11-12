253b843b81b2c82cb5374e5501208624d5c96568
Adrian Kocis <adrian.kocis@gmail.com>
2025-11-11 21:26:06 -0800

    Cosmetic yazi config cleanup

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -23,27 +23,27 @@ end, 500, Header.LEFT)
 
 -- show symlink targets in the bottom status
 Status:children_add(function(self)
-	local h = self._current.hovered
-	if h and h.link_to then
-		return " -> " .. tostring(h.link_to)
-	else
-		return ""
-	end
+    local h = self._current.hovered
+    if h and h.link_to then
+        return " -> " .. tostring(h.link_to)
+    else
+        return ""
+    end
 end, 3300, Status.LEFT)
 
 -- show user:group in the bottom status
 Status:children_add(function()
-	local h = cx.active.current.hovered
-	if not h or ya.target_family() ~= "unix" then
-		return ""
-	end
+    local h = cx.active.current.hovered
+    if not h or ya.target_family() ~= "unix" then
+        return ""
+    end
 
-	return ui.Line {
-		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-		":",
-		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-		" ",
-	}
+    return ui.Line {
+        ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+        ":",
+        ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+        " ",
+    }
 end, 500, Status.RIGHT)
 
 -- show file modification date in the bottom status
